872458a282c4bff01be8237154d8576b3d4acd4a
Fangge Deng <lessdfg@outlook.com>
2017-10-28 16:55:35 -0400

    don't suicide

--- contentScript.js
+++ contentScript.js
@@ -1,21 +1,16 @@
-chrome.browserAction.onClicked.addListener(function(tab){
-    chrome.tabs.executeScript({
-        // code: '';  grab code from FB page
 
-        var elements = document.querySelectorAll('[data-text="true"]');
-		var post = elements[0].innerHTML;
 
-		var words = new Set();
+var elements = document.querySelectorAll('[data-text="true"]');
+var post = elements[0].innerHTML;
 
-		words.add("suicide");
-		words.add("fml");
-		words.add("sucks");
+var words = new Set();
 
-		console.log(words);
+words.add("suicide");
+words.add("fml");
+words.add("sucks");
 
-		if (words.has(post)){
-			alert("Don't suicide");
-		}
-        
-    });
-});
+console.log(words);
+
+if (words.has(post)){
+	alert("Don't suicide");
+}
